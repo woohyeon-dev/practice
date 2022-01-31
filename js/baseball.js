47ccb5b29093b600f8a9f51e67298de3044a3757
woohyeon-dev <dngus3219@gmail.com>
2022-01-31 23:55:57 +0900

    Create all the functions and designs of the playboard of the game

--- js/baseball.js
+++ js/baseball.js
@@ -1,12 +1,16 @@
-const display = document.querySelector(".board__display");
-const buttons = Array.from(document.querySelectorAll(".board__btns"));
+const display = document.querySelector(".play-board__display");
+const buttons = Array.from(document.querySelectorAll(".play-board__btns"));
+const circle = document.getElementsByClassName("play-board__circle");
 
 let answer;
+let result = [0, 0, 0];
+let isFill = false;
 
 // 게임 초기화 시키는 함수
 function init() {
     answer = [];
     makingAnswer();
+    // answer=[1,2,3,4];
     display.value = "";
 }
 
@@ -19,21 +23,22 @@ function makingAnswer() {
             answer.push(n);
             i++;
         }
+        
+        // 중복된 수가 있는지 체크하는 함수 없으면 true 있으면 false
+        function notSame(n) {
+            return answer.every((e) => n !==e);
+        }
     }
 }
 
-// 중복된 수가 있는지 체크하는 함수 없으면 true 있으면 false
-function notSame(n) {
-    return answer.every((e) => n !==e);
-}
 
 // 정답과 사용자가 입력한 값을 비교하는 함수
 function compareAnswer(value) {
     // result 요소 차례대로 strike, ball, out
-    let result = [0, 0, 0];
+    result = [0, 0, 0];
     for(let i=0; i<4; i++) {
-        if(value.indexOf(answer[i])) {
-            if(value.charAt[i] == answer[i]) {
+        if(value.indexOf(answer[i])+1) {
+            if(value.charAt(i) == answer[i]) {
                 result[0]++;
             } else {
                 result[1]++;
@@ -42,12 +47,33 @@ function compareAnswer(value) {
             result[2]++;
         }
     }
-    showResult(result);
+    colorizeCircles();
 }
 
-// 결과를 화면에 나타내는 함수
-function showResult(result) {
-    console.log(result);
+// 결과를 통해 원 색깔 칠하는 함수
+function colorizeCircles() {
+    for(let i=0; i<result[0]; i++) {
+        circle[i].style.backgroundColor = "yellow";
+    }
+    for(let i=0; i<result[1]; i++) {
+        circle[i+4].style.backgroundColor = "green";
+    }
+    for(let i=0; i<result[2]; i++) {
+        circle[i+8].style.backgroundColor = "red";
+    }
+}
+
+// 원 색깔 초기화
+function initCircleColor() {
+    for(let i=0; i<result[0]; i++) {
+        circle[i].style.backgroundColor = "#505156";
+    }
+    for(let i=0; i<result[1]; i++) {
+        circle[i+4].style.backgroundColor = "#505156";
+    }
+    for(let i=0; i<result[2]; i++) {
+        circle[i+8].style.backgroundColor = "#505156";
+    }
 }
 
 // 버튼들의 이벤트 처리하는 함수
@@ -67,12 +93,16 @@ function handleOutput(e) {
             // ok버튼은 입력한 수가 서로 다른 네자리 수 일 때만 누를 수 있도록 만들어야함
             const valueArr = display.value.split("");
             const set = Array.from(new Set(valueArr));
-            if(set.length == 4) {
+            if(set.length == 4 && valueArr.length == 4) {
                 compareAnswer(display.value);
+                isFill = true;
+                display.value = "";
             }
-            display.value = "";
             break;
         default:
+            if(isFill) {
+                initCircleColor();
+            }
             // 전에 입력한 수와 다를 때만 입력되도록 만들어야함
             display.value += buttonValue;
             break;
